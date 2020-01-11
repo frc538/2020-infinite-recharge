83bcee5278e393a59ed8e83c7c3146fa2bba0391
HeathHudson <43255912+HeathHudson@users.noreply.github.com>
2020-01-10 19:47:52 -0600

    Defined the variables for the swerve module and started the constructor for the swerve module

--- src/main/java/frc/robot/subsystem/drive/SwerveModule.java
+++ src/main/java/frc/robot/subsystem/drive/SwerveModule.java
@@ -7,8 +7,23 @@
 
 package frc.robot.subsystem.drive;
 
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.SpeedController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 /**
  * Add your docs here.
  */
 public class SwerveModule {
+    private final SpeedController drive;
+    private final SpeedController turn;
+    private final AnalogInput driveEncoder;
+    private final AnalogInput turnEncoder;
+
+    public SwerveModule(int driveId, int turnId) {
+        drive = new CANSparkMax(driveId, MotorType.kBrushless);
+        turn = new CANSparkMax(turnId, MotorType.kBrushless);
+        
+    }
 }
